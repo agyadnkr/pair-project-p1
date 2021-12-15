a04a34f80d0fe8f7cfa4449e4f586145ff9fe2dc
Bimansyah Pratama <bimansyahpratama@gmail.com>
2021-12-15 23:36:10 +0700

    commit

---
+++ migrations/20211215163508-create-detail.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Details', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cc: {
+        type: Sequelize.INTEGER
+      },
+      mileage: {
+        type: Sequelize.INTEGER
+      },
+      transmission: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Details');
+  }
+};
